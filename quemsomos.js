10f6a3deed256c41a2d0762523b2b98fa1340b13
Pedro Julius <109457164+Pedro-SousaM@users.noreply.github.com>
2023-10-20 19:20:29 -0300

    Add files via upload

--- quemsomos.js
+++ quemsomos.js
@@ -2,5 +2,9 @@
 let subtitulo = document.querySelectorAll(".sub-tittle") 
 subtitulo.forEach((subtitulo)=>{
     console.log(subtitulo.className.split(" ")[1])
-    subtitulo.style.transform=`translateY(${10*Number(subtitulo.className.split(" ")[1])}vh)`
-}) 
+    subtitulo.style.transform=`translateY(${18*Number(subtitulo.className.split(" ")[1])}vh)`
+})  
+let change = document.querySelector(".p1") 
+change.addEventListener("click", ()=>{
+    change.style.order="3"
+})
