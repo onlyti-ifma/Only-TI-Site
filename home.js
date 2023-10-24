aee7483b98d60bf5c40bf007dc83fbfa1daf4b4b
Pedro Julius <109457164+Pedro-SousaM@users.noreply.github.com>
2023-10-24 02:34:42 -0300

    Add files via upload

--- home.js
+++ home.js
@@ -3,11 +3,11 @@ let contact_button= document.querySelector(".contact-button");
 contact_button.addEventListener("click",()=>{ 
     contact_button.style.transition="0.08s" 
     contact_button.style.right="20.4%" 
-    contact_button.style.bottom="15.5%"  
+    contact_button.style.bottom="11.5%"  
     contact_button.style.boxShadow="-6px 6px 4px rgba(0, 0, 0, 0.524)"
     setTimeout(()=>{
     contact_button.style.right="20%" 
-    contact_button.style.bottom="16%"  
+    contact_button.style.bottom="12%"  
     contact_button.style.transition="0.3s"  
     contact_button.style.boxShadow="-10px 10px 5px rgba(0, 0, 0, 0.524)"
 },100)
